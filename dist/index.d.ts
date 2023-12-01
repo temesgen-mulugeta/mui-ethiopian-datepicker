b0404ec560cabf4cbb6c549090b52e9f81040095
Temesgen <temesgenmulugeta11@gmail.com>
2023-12-01 15:02:34 +0300

    fixed export issue

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,5 +1,6 @@
 import EtDatePicker from "./EtDatePicker";
 import EtDateViewer from "./EtDateViewer";
 import { EthiopianDate } from "./util/EthiopianDateUtils";
+import { EtLocalizationProvider } from "./EtLocalizationProvider";
 export default EtDatePicker;
-export { EtDateViewer, EthiopianDate as EthiopianDateUtil };
+export { EtDateViewer, EthiopianDate as EthiopianDateUtil, EtLocalizationProvider, };
