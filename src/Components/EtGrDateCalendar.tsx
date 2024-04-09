54c41f1743a7c5be91e0eb6089029d5a98611d38
AbelTeha <abelteha@gmail.com>
2024-04-09 12:52:53 +0300

    This commit introduces some enhancements and fixes to the date picker:
    1. Refactored the naming convention of Amharic letters within the Amharic date component for clarity and consistency.
    2. Implemented a new feature: a button that allows users to easily select current date.
    3. Enhanced user experience by adding synchronized transitions between the Amharic and Gregorian dates.

--- src/Components/EtGrDateCalendar.tsx
+++ src/Components/EtGrDateCalendar.tsx
@@ -1,32 +1,72 @@
-import { Box, Divider } from "@mui/material";
+import { Box, Button, Divider } from "@mui/material";
 import { DateCalendar } from "@mui/x-date-pickers";
 
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import EthiopianDateCalendar from "./EthiopianDateCalendar";
 import { EtDatePickerContext } from "../EtDatePickerContext";
-import React from "react";
 
 const EtGrDateCalendar = () => {
   const etDatePickerContext = useContext(EtDatePickerContext);
-  const { value, onDateChange, disableFuture, disablePast, minDate, maxDate } =
-    etDatePickerContext;
+  const {
+    onDateChange,
+    disableFuture,
+    disablePast,
+    minDate,
+    maxDate,
+    onMonthChange,
+    gregDate,
+    setGregDate,
+  } = etDatePickerContext;
+
+  const gregDateValue = gregDate?.toLocaleDateString();
+
+  const [gregDatePicker, setGregDatePicker] = useState<Date | null>(null);
+
+  useEffect(() => {
+    if (gregDateValue) {
+      const value = new Date(gregDateValue);
+      setGregDatePicker(value);
+    }
+  }, [gregDateValue]);
+
+  const handleTodayButtonClick = () => {
+    onDateChange(new Date());
+  };
 
   return (
     <Box sx={{ width: 600 }}>
       <Box display={"flex"}>
-        <Box width={295}>
+        <Box width={295} display="flex" flexDirection="column">
           <EthiopianDateCalendar />
+
+          <Box
+            sx={{
+              flexGrow: 1,
+              display: "flex",
+              alignItems: "flex-end",
+            }}
+          >
+            <Button sx={{ ml: 2 }} onClick={handleTodayButtonClick}>
+              Today
+            </Button>
+          </Box>
         </Box>
         <Divider orientation="vertical" flexItem />
         <Box width={295}>
           <DateCalendar
             monthsPerRow={3}
-            value={value ?? null}
+            value={gregDatePicker}
             onChange={(date) => {
               if (date && date instanceof Date) onDateChange(date);
             }}
             sx={{ mr: 1 }}
             disableFuture={disableFuture}
+            onMonthChange={(date) => {
+              const newDate = new Date(date);
+              newDate.setDate(gregDate?.getDate() ?? 15);
+              onMonthChange(newDate);
+              setGregDate(newDate);
+            }}
             disablePast={disablePast}
             minDate={minDate}
             maxDate={maxDate}
--- src/EtDatePickerContext.tsx
+++ src/EtDatePickerContext.tsx
@@ -2,14 +2,23 @@ import React, { useState, createContext, ReactNode, useEffect } from "react";
 import { EtDateFieldProps } from "./EtDatePicker";
 import { LocalizationProvider } from "@mui/x-date-pickers";
 import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
+import { set } from "date-fns";
 
 type EtDatePickerContextType = {
   value?: Date;
+  monthValue?: Date;
+  gregDate?: Date;
+  setGregDate: (date: Date) => void;
+  onMonthChange: (date: Date) => void;
   onDateChange: (date: Date) => void;
 } & EtDateFieldProps;
 
 const EtDatePickerContext = createContext<EtDatePickerContextType>({
   value: new Date(),
+  monthValue: new Date(),
+  gregDate: new Date(),
+  setGregDate: (date: Date) => {},
+  onMonthChange: (date: Date) => {},
   onDateChange: (date: Date) => {},
 });
 
@@ -30,15 +39,22 @@ const EtDatePickerProvider: React.FC<EtDatePickerProviderProps> = ({
   maxDate,
 }) => {
   const [date, setDate] = useState<Date>();
+  const [monthValue, setMonthValue] = useState<Date>();
+  const [gregDate, setGregDate] = useState<Date>();
 
   const onDateChange = (date: Date) => {
     setDate(date);
     onChange?.(date);
   };
 
+  const onMonthChange = (date: Date) => {
+    setMonthValue(date);
+  };
+
   useEffect(() => {
     if (value) {
       setDate(value);
+      setGregDate(value);
     }
   }, [value]);
 
@@ -47,6 +63,10 @@ const EtDatePickerProvider: React.FC<EtDatePickerProviderProps> = ({
       <EtDatePickerContext.Provider
         value={{
           value: date ?? undefined,
+          monthValue: monthValue,
+          gregDate: gregDate,
+          setGregDate,
+          onMonthChange,
           onDateChange,
           disableFuture,
           disablePast,
